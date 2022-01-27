4a06579bfaa29592abd965fa6a4341b5edd708d2
Nayan Biswas <nayan32biswas@gmail.com>
2022-01-27 17:12:50 +0600

    learn auth

---
+++ exception_handler.py
@@ -0,0 +1,21 @@
+from fastapi import (
+    FastAPI,
+    Request,
+)
+from fastapi.responses import JSONResponse
+
+
+app = FastAPI()
+
+
+class UnicornException(Exception):
+    def __init__(self, name: str):
+        self.name = name
+
+
+@app.exception_handler(UnicornException)
+async def unicorn_exception_handler(request: Request, exc: UnicornException):
+    return JSONResponse(
+        status_code=418,
+        content={"message": f"Oops! {exc.name} did something. There goes a rainbow..."},
+    )
--- main.py
+++ main.py
@@ -1,43 +1,45 @@
-from datetime import datetime
-from typing import Optional, List
+from typing import Optional
+from datetime import datetime, timedelta
 
 from fastapi import (
     Cookie,
+    Depends,
     FastAPI,
-    File,
-    Form,
     Header,
     HTTPException,
-    Path,
-    Query,
-    Request,
     status,
-    UploadFile,
 )
-from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 
-
-class PostIn(BaseModel):
-    name: str = Field(..., min_length=5, max_length=250)
-    description: Optional[str] = None
-    published_at: datetime = None
-    number: int = 0
-
-
-class PostOut(PostIn):
-    id: int = None
-
-
-class PostForm(PostIn):
-    image: UploadFile = File(...)
+from jose import JWTError, jwt
+from passlib.context import CryptContext
 
 
 app = FastAPI()
+# Global dependency
+# app = FastAPI(dependencies=[Depends(verify_token), Depends(verify_key)])
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
-@app.get("/", status_code=status.HTTP_200_OK)
+@app.get(
+    "/",
+    status_code=status.HTTP_200_OK,
+)
 def home(cookie: Optional[str] = Cookie(None), user_agent: Optional[str] = Header(None)):
     return {
         "message": "Hello World",
@@ -46,86 +48,150 @@ def home(cookie: Optional[str] = Cookie(None), user_agent: Optional[str] = Heade
     }
 
 
-@app.post("/api/v1/posts/", response_model=PostOut, status_code=status.HTTP_201_CREATED)
-def post_create(post: PostIn):
-    return post
+fake_users_db = {
+    "nayan": {
+        "username": "nayan",
+        "full_name": "Nayan Biswas",
+        "email": "nayan@example.com",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "disabled": False,
+    },
+    "alice": {
+        "username": "alice",
+        "full_name": "Alice Wonderson",
+        "email": "alice@example.com",
+        "hashed_password": "fakehashedsecret2",
+        "disabled": True,
+    },
+}
 
 
-@app.post("/api/v1/posts-form/")
-def post_create_with_form(
-    image: UploadFile = File(...),
-    name: str = Form(...),
-    description: Optional[str] = Form(None),
-    published_at: datetime = Form(None),
-    number: int = Form(0),
-):
-    return {
-        "image": image.filename,
-        "name": name,
-        "description": description,
-        "published_at": published_at,
-        "number": number,
-    }
+class User(BaseModel):
+    username: str
+    email: Optional[str] = None
+    full_name: Optional[str] = None
+    disabled: Optional[bool] = None
 
 
-# @app.post("/api/v1/posts-form/")
-# def post_create_with_form(post: PostForm):
-#     return {"image": "form"}
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
 
-@app.get("/api/v1/posts/", status_code=status.HTTP_200_OK)
-def posts(
-    user_id: int = None,
-    limit: int = 10,
-    page: int = 1,
-    query: Optional[str] = Query(
-        None,
-        min_length=5,
-        max_length=10,
-        regex="^post.*$",
-        title="Query string",
-        alias="q",
-    ),
-    order_by: Optional[List[str]] = Query(
-        ["key", "direction"],
-        max_length=10,
-        deprecated=True,
-    ),
-    hidden_query: Optional[str] = Query(None, include_in_schema=False),
-):
-    return {
-        "params": f"user_id={user_id}, query={query}, limit={limit}, page={page}, order_by={order_by}, hidden_query={hidden_query}",
-    }
+class TokenData(BaseModel):
+    username: Optional[str] = None
 
 
-@app.get("/api/v1/posts/{post_id}")
-def post_details(*, post_id: int = Path(0, ge=0, le=1000, title="Post ID"), query: str):
-    # By adding * you can order parameters
-    if post_id == 0:
-        raise HTTPException(
-            status_code=404,
-            detail="Item not found",
-            headers={"X-Error": "There goes my error"},
-        )
-    return {"id": post_id, "description": f"Post details of '{post_id}'"}
+def fake_hash_password(password: str):
+    print(password)
+    return "fakehashed" + password
 
 
-@app.post("/upload-image/")
-async def create_upload_file(file: UploadFile = None):
-    if not file:
-        return {"message": "No upload file sent"}
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# to get a string like this run:
+# openssl rand -hex 32
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+def verify_password(plain_password, hashed_password):
+    print("\n\nplain_password:", plain_password)
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    print("\n\nget_password_hash:", password)
+    return pwd_context.hash(password)
+
+
+def get_user(db, username: str):
+    print("\n\nget_user:", username)
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+def authenticate_user(fake_db, username: str, password: str):
+    print("\n\nauthenticate_user", username)
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    print("\n\ncreate_access_token", expires_delta)
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
     else:
-        return {"filename": file.filename}
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
 
-class UnicornException(Exception):
-    def __init__(self, name: str):
-        self.name = name
-
-
-@app.exception_handler(UnicornException)
-async def unicorn_exception_handler(request: Request, exc: UnicornException):
-    return JSONResponse(
-        status_code=418,
-        content={"message": f"Oops! {exc.name} did something. There goes a rainbow..."},
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    print("\n\nget_current_user", token)
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
     )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    print("\n\nget_current_active_user")
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+@app.post("/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    # Extend or reimplement OAuth2PasswordRequestForm to change authentication format or add additional fields
+    print("\n\nlogin_for_access_token")
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@app.get("/users/me/", response_model=User)
+async def read_users_me(current_user: User = Depends(get_current_active_user)):
+
+    return current_user
+
+
+@app.get("/users/me/items/")
+async def read_own_items(current_user: User = Depends(get_current_active_user)):
+    return [{"item_id": "Foo", "owner": current_user.username}]
